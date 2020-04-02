7d6bf04fbec4c7875ab9225ec6b8617fddd9a544
Michael Koeppl <Michael.Koeppl@msg.group>
2020-04-02 16:34:07 +0200

    Add clone of keycloak

--- helper.sh
+++ helper.sh
@@ -128,6 +128,7 @@ function helper_git_download_all() {
         helper_git_download https://github.com/Frickeldave/docker_gitea "docker_gitea"
         helper_git_download https://github.com/Frickeldave/docker_java "docker_java"
         helper_git_download https://github.com/Frickeldave/docker_jenkins "docker_jenkins"
+        helper_git_download https://github.com/Frickeldave/docker_keycloak "docker_keycloak"
     else
         log "skip git update"
     fi
