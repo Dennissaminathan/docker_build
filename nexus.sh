0ebaad55a38b31d534c0656fba74eebe71e64bbd
Dennis Saminathan <Dennis.Saminathan@msg-global.com>
2020-04-28 18:12:24 +0530

    Added password change script

--- nexus.sh
+++ nexus.sh
@@ -5,8 +5,8 @@ function nexus_initial_setup() {
     log "wait for nexus availability"
     nexus_wait_for_startup
 
-#    log "create nexus realm"
-#    nexus_create_realm
+#    log "change nexus initial password"
+#    nexus_change_password
 
     log "create nexus testusers"
     nexus_create_users
@@ -22,16 +22,93 @@ function nexus_wait_for_startup() {
     log "check Nexus availability by searching for firststart_finished.flg"
     cmd="ls /home/appuser/data/firststart_finished.flg"
 
-    #while ! docker exec -it ${MC_PROJECT}_nexus_1 sh -c "${cmd}" > /dev/null 2>&1; do
-    #    log "firststart_finished.flg not available. Wait 15s for slow java junk and try again ..."
-    #    sleep 15s
-    #done
+    while ! docker exec -it ${MC_PROJECT}_nexus_1 sh -c "${cmd}" > /dev/null 2>&1; do
+        log "firststart_finished.flg not available. Wait 15s for slow java junk and try again ..."
+        sleep 15s
+    done
 
     log "nexus setup finished. Try to login now to check if nexus is available."
+
+    log "Check if nexus is accepting requests"
+
+    cmd="curl -I https://localhost:${NX_INTPORT} --insecure 2>/dev/null | head -n 1 | cut -d$' ' -f2"
+
+    cnt=0
+        while ! docker exec -it ${MC_PROJECT}_nexus_1 sh -c "${cmd}" ; do
+	((cnt++))
+        log "Nexus service is not available. Wait 30s and try again ..."
+        sleep 20s
+        if [ $cnt -eq 10 ]
+        then
+            log "Failed to start Nexus"
+            exit 1
+        fi
+    done
 
 
     MC_LOGINDENT=$((MC_LOGINDENT-3))
 }
+
+function nexus_change_password() {
+
+    cmd="ls /home/appuser/data/firststart.flg"
+    while ! docker exec -it ${MC_PROJECT}_nexus_1 sh -c "${cmd}" > /dev/null 2>&1; do
+        echo "firststart_finished.flg not available. Wait 15s for slow java junk and try again ..."
+        sleep 10s
+    done
+   NEXUS_OLD_PWD=$(docker exec -it ${MC_PROJECT}_nexus_1 sh -c  "cat /home/appuser/data/sonatype-work/nexus3/admin.password")
+
+
+   NEXUS_URL=https://172.6.66.114:${NX_INTPORT}
+   NEXUS_NEW_PWD=admin123
+   shift 3
+   CURL_OPTS="$@"
+   SCRIPT_NAME=change_admin_password
+   read -r -d '' SCRIPT_JSON << EOF
+{
+  "name": "${SCRIPT_NAME}",
+  "type": "groovy",
+  "content": "security.securitySystem.changePassword('admin', args)"
+}
+EOF
+
+   CHECK_SCRIPT_STATUS=`curl ${CURL_OPTS} -s -o /dev/null -I -w "%{http_code}" -u "admin:${NEXUS_OLD_PWD}" "${NEXUS_URL}/service/siesta/rest/v1/script/${SCRIPT_NAME}"`
+
+   echo $SCRIPT_JSON
+#### Below curl posts would reset the admin password but initial password which is created by Nexus is stored at "/home/appuser/data/sonatype-work/nexus3/admin.password" inside container which needs to be changed only in the GUI of nexus using wizard. Initial password is currently unable to reset using below script. i
+### If the initial password is reset using GUI then below script can be used to reset the admin password.
+
+   echo $CHECK_SCRIPT_STATUS
+   echo $CURL_OPTS
+
+   if [ "${CHECK_SCRIPT_STATUS}" == "404" ];then
+    echo "> ${SCRIPT_NAME} is not found (${CHECK_SCRIPT_STATUS})"
+    echo "> creating script (${SCRIPT_NAME}) ..."
+    curl ${CURL_OPTS} -H "Accept: application/json" -H "Content-Type: application/json" -d "${SCRIPT_JSON}" -u"admin:${NEXUS_OLD_PWD}" "${NEXUS_URL}/service/siesta/rest/v1/script/" --insecure
+   elif [ "${CHECK_SCRIPT_STATUS}" == "401" ];then
+    echo "> Unauthorized (${CHECK_SCRIPT_STATUS})"
+     return
+   else
+    echo "> ${SCRIPT_NAME} is found (${CHECK_SCRIPT_STATUS})"
+    echo "> updating script (${SCRIPT_NAME}) ..."
+
+### Nexus scripts should be uploaded to "/service/siesta/rest/v1/script" location before run it. All configuration scripts should be uploaded first before execution.   
+    curl ${CURL_OPTS} -XPUT -H "Accept: application/json" -H "Content-Type: application/json" -d "${SCRIPT_JSON}" -u "admin:${NEXUS_OLD_PWD}" "${NEXUS_URL}/service/siesta/rest/v1/script/${SCRIPT_NAME}" --insecure
+  fi
+
+
+  echo "> updating password ..."
+### Uploaded scripts can be executed like below curl post command with credential.
+
+  CHECK_RUN_STATUS=`curl ${CURL_OPTS} -s -o /dev/null -w "%{http_code}" -H "Content-Type: text/plain" -u "admin:${NEXUS_OLD_PWD}" "${NEXUS_URL}/service/siesta/rest/v1/script/${SCRIPT_NAME}/run" -d "${NEXUS_NEW_PWD}" --insecure`
+
+  if [ "${CHECK_RUN_STATUS}" == "200" ];then
+    echo "> succeeded!"
+  else
+    echo "> failed! (${CHECK_RUN_STATUS})"
+  fi
+}
+
 
 function nexus_create_users() {
 
@@ -84,6 +161,7 @@ function nexus_create_users() {
             esac
             shift
             ((cnt++))
+	    ### Nexus user creation does not accept bulk users in a json file so json should have one user at a time.
             ##nexus_write_user "${u}" "${user_firstname}" "${user_lastname}" "${user_mailusername}" "${user_maildomain}" "${user_password}"
         done
     done
